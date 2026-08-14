8d68b3c7d74cb474927eca123c38a60559a9be09
Gamerinp <irwinmesa@gmail.com>
2026-08-14 12:47:36 -0700

    creacion de funciones de busqueda

--- opciones.py
+++ opciones.py
@@ -1,5 +1,34 @@
-count_falied = 0
-count_success = 0
-set_usuarios = set()
-lista_sospechosos = []
-dict_ip = {}
+import recepcion_datos
+def buscar_ip(ip:str):
+    count= {"failed":0, "success":0}
+    eventos = recepcion_datos.almacenar_logs("logs.txt") #[{'fecha_hora': '2026-08-11 08:00:12', 'usuario': 'admin', 'ip': '192.168.1.20', 'resultado': 'FAILED'}
+    for evento in eventos:
+        if evento["ip"] == ip and evento["resultado"] == "FAILED":
+                count["failed"] += 1
+        elif evento["ip"] == ip and evento["resultado"] == "SUCCESS":
+                count["success"] += 1
+    return count
+
+def buscar_user(user:str):
+    count= {"failed":0, "success":0}
+    eventos = recepcion_datos.almacenar_logs("logs.txt") #[{'fecha_hora': '2026-08-11 08:00:12', 'usuario': 'admin', 'ip': '192.168.1.20', 'resultado': 'FAILED'}
+    for evento in eventos:
+        if evento["usuario"] == user and evento["resultado"] == "FAILED":
+                count["failed"] += 1
+        elif evento["usuario"] == user and evento["resultado"] == "SUCCESS":
+                count["success"] += 1
+    return count
+
+def buscar_resultado(op:int):
+    usuarios_failed = set()
+    usuarios_succes = set()
+    eventos = recepcion_datos.almacenar_logs("logs.txt") #[{'fecha_hora': '2026-08-11 08:00:12', 'usuario': 'admin', 'ip': '192.168.1.20', 'resultado': 'FAILED'}
+    for evento in eventos:
+        if op == 1:
+            if evento["resultado"] == "FAILED":
+                usuarios_failed.add(evento["usuario"])
+        elif op == 2:
+            if evento["resultado"] == "SUCCESS":
+                usuarios_succes.add(evento["usuario"])
+    if op == 1: return usuarios_failed
+    elif op == 2: return usuarios_succes
--- recepcion_datos.py
+++ recepcion_datos.py
@@ -1,34 +1,19 @@
 
-def analizar_logs(archivo_logs: str):
-    cantidad_lineas = 0
+def almacenar_logs(archivo_logs: str):
     # 'r' significa modo lectura (read)
     # encoding='utf-8' es una buena práctica para evitar problemas con acentos o caracteres
     # usar la instruccion with permite cerrar el archivo automaticamente (buena practica)
-    with open(archivo_logs, "r", encoding="utf-8") as archivo:
+    eventos = []
+    with open("logs.txt", "r", encoding="utf-8") as archivo:
         for linea in archivo:
-            cantidad_lineas += 1
-            # .strip() quita el salto de línea '\n' al final de cada texto
-            linea_limpia = linea.strip()
-            #convertimos la linea de log en una lista separada por palabras
-            lista_como_linea = linea_limpia.split()
-            #separamos los logs con dato correcto, 8 elementos como lista
-            if len(lista_como_linea) == 8:
-                set_usuarios.add(lista_como_linea[3])
-                dict_ip.update({lista_como_linea[3]:lista_como_linea[5]})
-
-                if lista_como_linea[7] == "FAILED":
-                    count_falied += 1
-                    lista_sospechosos.append(lista_como_linea[3])
-                else:
-                    count_success += 1
-
-    print('====== SECURITY REPORT ======\n')
-    print(f'total de eventos: {cantidad_lineas}\n')
-    print(f'Logs fallidos: {count_falied}')
-    print(f'Logs exitosos: {count_success}\n')
-    for nombre in set_usuarios:
-        if lista_sospechosos.count(nombre) >= 3:
-            print(f'usuario sospechoso: {nombre} con {lista_sospechosos.count(nombre)} logs fallidos')
-            print(f'Ultima ip de acceso: {dict_ip[nombre]}')
-            print("posible ataque de fuerza bruta")
-    print('=============================')
+            partes = linea.strip().split(" | ")  # Usar " | " te da 4 partes limpias
+            if len(partes) == 4:
+                fecha_hora, usuario, ip, resultado = partes
+                # Guardas cada log como un diccionario sencillo
+                eventos.append({
+                    "fecha_hora": fecha_hora,
+                    "usuario": usuario,
+                    "ip": ip,
+                    "resultado": resultado
+                })
+    return eventos
